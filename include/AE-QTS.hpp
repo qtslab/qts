7aaa71086870135f3874d44aa096742bef605b80
young <young20050727@gmail.com>
2023-12-09 21:28:21 +0800

    AE-QTS done

--- include/AE-QTS.hpp
+++ include/AE-QTS.hpp
@@ -22,13 +22,13 @@ int AE_QTS(items_t& items, double capacity, int max_gen, int N) {
             adjust_solution(items, neighbors[j], capacity);
         }
 
-        std::vector<solution_t> sorted_neighbors = sort_solution(items, neighbors, N);
+        std::vector<solution_t> sorted_neighbors = sort_solution(items, neighbors, N); // from best(index 0) to worst(index N-1)
         if (calculate_values(items, sorted_neighbors[0]) > calculate_values(items, best_fit)) {
             best_fit = sorted_neighbors[0];
         }
 
         for (int j=0; j<N/2; j++) {
-            update_q(sorted_neighbors[j], sorted_neighbors[(question_size-1)-j], qindividuals, 0.01/(j+1));
+            update_q(sorted_neighbors[j], sorted_neighbors[(N-1)-j], qindividuals, 0.01/(j+1));
         }
 
         // print_solution(items, qindividuals, best_fit); // debug
---
+++ include/case.hpp
@@ -0,0 +1,58 @@
+#ifndef CASE_HPP
+#define CASE_HPP
+
+#include <random>
+
+#include "type.h"
+#include "constant.h"
+
+int case_I(items_t& items, double capacity, int max_gen, int N) {
+    items_t items;
+    for (int i=0; i<question_size; i++) {
+        int value = (i % 10) + 1;
+        items[i].weight = value;
+        items[i].value = value + 5;
+    }
+
+    for (int i=0; i<question_size; i++) {
+        capacity += items[i].weight;
+    }
+
+    capacity /= 2;
+
+    return 0;
+}
+
+int case_II(items_t& items, double capacity, int max_gen, int N) {
+    items_t items;
+    std::random_device rd;  // 取得隨機數種子
+    std::mt19937 gen(rd()); // 使用 Mersenne Twister 引擎
+    for (int i=0; i<question_size; i++) {
+        std::uniform_real_distribution<double> value_dis(min_value, max_value);
+        std::uniform_real_distribution<double> weight_dis(min_weight, max_weight);
+        items[i].weight = weight_dis(gen);
+        items[i].value = items[i].weight + 5;
+    }
+
+    for (int i=0; i<question_size; i++) {
+        capacity += items[i].weight;
+    }
+
+    capacity /= 2;
+
+    return 0;
+}
+
+int case_III(items_t& items, double capacity, int max_gen, int N) {
+    items_t items;
+
+    for (int i=0; i<question_size; i++) {
+        capacity += items[i].weight;
+    }
+
+    capacity /= 2;
+
+    return 0;
+}
+
+#endif
