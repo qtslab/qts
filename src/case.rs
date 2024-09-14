739f77ca9a7b2eafe49103fd9d6fd40b9c81c55f
Young <young20050727@gmail.com>
2024-09-15 00:58:46 +0800

    feat: Add case_II and case_III

--- src/case.rs
+++ src/case.rs
@@ -15,3 +15,32 @@ pub fn case_I(items: &mut Items, mut problem: problem) -> f64 {
 
     return problem.capacity / 2.0;
 }
+
+pub fn case_II(items: &mut Items, mut problem: problem) -> f64 {
+    // weight = i%10 + 1
+    // value = random(min_value, max_value)
+    let mut rng = rand::thread_rng();
+    items.resize(problem.size as usize, Default::default());
+    for i in 0..problem.size {
+        items[i as usize].weight = i as f64/10.0 + 1.0;
+        items[i as usize].value = rng.gen_range(problem.min_value..problem.max_value);
+        problem.capacity += items[i as usize].weight;
+    }
+
+    return problem.capacity / 2.0;
+}
+
+
+pub fn case_III(items: &mut Items, mut problem: problem) -> f64 {
+    // weight = i%10 + 1
+    // value = weight + 5
+    let mut rng = rand::thread_rng();
+    items.resize(problem.size as usize, Default::default());
+    for i in 0..problem.size {
+        items[i as usize].weight = i as f64%10.0 + 1.0;
+        items[i as usize].value = items[i as usize].weight + 5.0;
+        problem.capacity += items[i as usize].weight;
+    }
+
+    return problem.capacity / 2.0;
+}
